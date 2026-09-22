5dd765adf4154cadf17c66f111f9450ebfb24b41
Leandro Verdun <redengel15@gmail.com>
2026-09-22 20:21:14 -0300

    feat: integrate flash sale into homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import Carrusel from "../components/Carrusel";
 import Cards from "../components/Cards";
 import NewProducts from "../components/NewProducts";
 import FeaturedProducts from "../components/FeaturedProducts";
+import Newsletter from "../components/Newsletter";
+import FlashSale from "../components/FlashSale";
 
 function Home() {
   return (
@@ -9,9 +11,11 @@ function Home() {
       <Carrusel />
 
       <main>
-        <Cards />
+        <FlashSale />     
         <NewProducts />
+        <Cards />
         <FeaturedProducts />
+        <Newsletter />
       </main>
     </>
   );
